bb1755aefee2d03f4fb572cffb1f597e87334b6e
jason kirtland <jek@discorporate.us>
2011-01-27 16:02:00 -0800

    Version 0.2.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 readme = codecs.open('README', 'r', 'utf-8').read().encode('utf-8')
 
 setup(name='translitcodec',
-      version='0.1',
+      version='0.2',
       description='Unicode to 8-bit charset transliteration codec',
       long_description=readme,
       author='Jason Kirtland',
